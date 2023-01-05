2696b9aa1cd90267e885c7e168737c97411f33cd
Kiara Diaz <diazdiazkiarakaxandra@gmail.com>
2023-01-04 23:13:01 -0300

    constantes y lets

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+//variables y constantes
+
+const nombre = 'Kiara';
+let apellido = 'Diaz';
+
+//si tengo planes de cambiar el valor lo dejo como let, sino como const
+let valorDado = 5;
+//let crea variables de scot, se pueden volver a crear en funciones locales sin mutar el original
+
+if ( true ) {
+    const nombre = 'Peter'
+    let valorDado = 6;
+    console.log(nombre, valorDado)
+}
